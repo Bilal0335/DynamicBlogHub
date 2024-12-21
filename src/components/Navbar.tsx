f41b456217eefaaac86a4b5c8dc1a2bf7ac310e6
Bilal0335 <bilalhussain42201.com>
2024-12-21 13:28:28 +0500

    😄 update 😄

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [currentDateTime, setCurrentDateTime] = useState(``);
+  useEffect(() => {
+    const upDateTime = () => {
+      const now = new Date();
+      const options: Intl.DateTimeFormatOptions = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false
+      };
+      setCurrentDateTime(
+        now.toLocaleDateString("en-US", options) +
+          "" +
+          now.toLocaleTimeString("en-US", { hour12: false })
+      );
+    };
+    upDateTime();
+    const interValId = setInterval(upDateTime, 1000);
+    return () => clearInterval(interValId);
+  }, []);
 
-export default Navbar
+  return (
+    <>
+      <header className="text-gray-400 shadow-lg">
+        <div className="mx-auto flex flex-wrap flex-col md:flex-row p-2 items-center container">
+          <div className="flex-1 text-black">
+            <span>{currentDateTime}</span>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
